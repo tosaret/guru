477b469cb40c40662d65be084a70d2fccf69038a
Denys Zabohonskyi <2saret@gmail.com>
2018-06-25 20:49:39 +0300

    animations

--- app.js
+++ app.js
@@ -7,3 +7,30 @@ var the_id = $(this).attr("href");
     }, 'slow');
 
 return false;});
+
+function cssParallax(cont, el, radiusVal){
+  $(cont).mousemove(function(event) {
+      
+      cx = Math.ceil($(window).width() / 1.0);
+      cy = Math.ceil($(window).height() / 1.0);
+      dx = event.pageX - cx;
+      dy = event.pageY - cy;
+      
+      tiltx = (dy / cy);
+      tilty = - (dx / cx);
+      radius = Math.sqrt(Math.pow(tiltx,2) + Math.pow(tilty,2));
+      degree = (radius * radiusVal);
+
+      $(el, cont).css('-webkit-transform','rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
+      $(el, cont).css('transform','rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)');
+  });
+}
+
+$(document).ready(function() {
+  cssParallax('.guru-parallax', '.parallax-base', 6);
+
+  $('.caption').mouseover(function() {
+  	$('.caption-active').fadeOut();
+  	$('.caption-active-'+$(this).attr('data-caption')).fadeIn();
+  });
+});
